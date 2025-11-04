c82a36207822a14858369fa17e2e4ca72441c24e
Diego-BS <brito.diegow@gmail.com>
2025-11-03 20:08:11 -0600

    Agrega proyecto vacio

---
+++ ch3-ex1/src/test/java/com/diego/ch3_ex1/Ch3Ex1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.diego.ch3_ex1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ch3Ex1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
